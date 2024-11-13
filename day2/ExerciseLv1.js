9577ad43bdaa12f6b551b4e529b734c58fdf6174
Phát <binkhumno01@gmail.com>
2024-11-13 16:53:13 +0700

    it test

---
+++ day2/ExerciseLv1.js
@@ -0,0 +1,39 @@
+let challange ="30 day"
+let Test="Test for something"
+let companies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+let newword='You cannot end a sentence with because because because is a conjunction'
+let A = "30 days of"
+let B = "JavaScript"
+let task="30 Days Of JavaScript"            //1
+
+console.log(challange)                      //2
+console.log(challange.length)               //3
+console.log(Test.toUpperCase())               //4
+console.log(Test.toLowerCase())               //5
+console.log(Test.substr(9,14))              //6
+console.log(task.substring(3,21))           //7
+console.log(challange.includes("Script"))   //8
+console.log(task.includes("Script"))        //8
+console.log(Test.split())                   //9
+console.log(task.split(" "))                //10
+console.log(companies.split(','))           //11
+console.log(task.replace("JavaScript","Python")) //12
+console.log(task.charAt("15"))              //13
+console.log(task.charCodeAt("11"))          //14
+console.log(task.indexOf("a"))              //15
+console.log(task.lastIndexOf("a"))              //16
+console.log(newword.indexOf("because"))     //17
+console.log(newword.lastIndexOf("because")) //18
+console.log(newword.search("beacuse"))      //19
+console.log(task.trim(' '))                 //20  
+console.log(task.startsWith("30"))          //21
+console.log(task.endsWith("Script"))        //22
+console.log(task.match(/a/g))               //23
+console.log(A.concat(" ",B))                //24
+console.log((A + " ").repeat(2).trim());
+
+
+
+
+
+
